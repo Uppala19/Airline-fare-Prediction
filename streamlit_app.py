6acff8da0fdfb6683c88ac14c2fa17550a23bca7
Uppala19 <160552325+Uppala19@users.noreply.github.com>
2025-04-11 06:02:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -415,7 +415,8 @@ if data is not None:
         # Price vs. Number of Stops
         st.subheader("Price vs. Number of Stops")
         fig_scatter, ax_scatter = plt.subplots(figsize=(10, 6))
-        sns.scatterplot(x="Price", y="Total_Stops",s=80, data=data, alpha=0.7,color='viridis',edgecolor="black)
+        sns.scatterplot(x="Price", y="Total_Stops",s=80, data=data,c="Price", alpha=0.7,color='viridis',edgecolor="black)
+        st.colorbar(label="Price")
         st.pyplot(fig_scatter)
 
         # Price Distribution
